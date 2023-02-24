d021a7c9b66b11582cac5f086313db9b2e9fa922
Faixifk <35093954+Faixifk@users.noreply.github.com>
2023-02-24 15:32:48 +0500

    Update NaiveBayes.py

--- ML_tests/NaiveBayes_tests/NaiveBayes.py
+++ ML_tests/NaiveBayes_tests/NaiveBayes.py
@@ -14,6 +14,7 @@ from naivebayes import NaiveBayes
 
 
 class NaiveBayes(unittest.TestCase):
+
     def setUp(self):
         # test cases we want to run
         self.num_examples = 3
@@ -21,58 +22,6 @@ class NaiveBayes(unittest.TestCase):
         self.num_classes = 3
         self.eps = 1e-6
 
-    def test_correctShape(self):
-
-        instance = NaiveBayes(np.array(
-
-            [
-                [1,1,1,4,6],
-                [2,3,3,3,6],
-                [4,6,7,4,7]
-            ]
-
-        ), [1,1,1,3,5,6,7,3,5,7,6])
-
-        self.assertTrue((instance.num_examples, instance.num_classes == (self.num_examples, self.num_features))
-
-    def test_correctNumFeatures(self):
-
-        instance = NaiveBayes(np.array(
-
-            [
-                [1,1,1,4,6],
-                [2,3,3,3,6],
-                [4,6,7,4,7]
-            ]
-
-        ), [1,1,1,3,5,6,7,3,5,7,6])
-        self.assertTrue(instance.num_features == self.num_features)
-
-    def test_correctNumExamples(self):
-
-        instance = NaiveBayes(np.array(
-
-            [
-                [1,1,1,4,6],
-                [2,3,3,3,6],
-                [4,6,7,4,7]
-            ]
-
-        ), [1,1,1,3,5,6,7,3,5,7,6])
-        self.assertTrue(instance.num_examples == self.num_examples)
-
-    def test_correctEps(self):
-
-        instance = NaiveBayes(np.array(
-
-            [
-                [1,1,1,4,6],
-                [2,3,3,3,6],
-                [4,6,7,4,7]
-            ]
-
-        ), [1,1,1,3,5,6,7,3,5,7,6])
-        self.assertTrue(instance.eps == self.eps)
 
 if __name__ == "__main__":
     print("Running Naive Bayes tests:")
